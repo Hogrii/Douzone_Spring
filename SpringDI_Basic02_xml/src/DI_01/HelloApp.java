4108c33923b8f320510aa82332446cd3f706aa05
ljh <hosboy93@naver.com>
2023-06-12 18:02:53 +0900

    자바 객체 선언, 메소드 호출

---
+++ SpringDI_Basic02_xml/src/DI_01/HelloApp.java
@@ -0,0 +1,18 @@
+package DI_01;
+
+public class HelloApp {
+	public static void main(String[] args) {
+		MessageBean messageBean = new MessageBean();
+		messageBean.sayHello("hong");
+	}
+}
+/*
+	요구사항
+	1. 한글버전 (hong) >> 안녕 hong
+	2. 영문버전 (hong) >> hello hong
+	
+	MessageBean_kr 
+	MessageBean_en
+	
+	interface 다형성 설계
+ */
